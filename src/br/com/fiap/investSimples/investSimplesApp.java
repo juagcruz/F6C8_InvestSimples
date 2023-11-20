6867e43b118c3d26b256dc5ed89d7f53f42c1b1f
juagcruz <juarez.garcia@gmail.com>
2023-11-20 01:39:57 -0300

    fix: check for listaEntrada

--- src/br/com/fiap/investSimples/investSimplesApp.java
+++ src/br/com/fiap/investSimples/investSimplesApp.java
@@ -6,7 +6,6 @@ import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.Calendar;
 import java.util.List;
-
 import br.com.fiap.investSimples.model.T_cliente;
 import br.com.fiap.investSimples.model.T_entradas;
 import dao.EntradasDao;
@@ -16,7 +15,7 @@ public class investSimplesApp {
 
 	public static void main(String[] args) {
 
-		T_entradas entrada = new T_entradas();
+//		T_entradas entrada = new T_entradas();
 
 		entrada.setTipo("Salario");
 		entrada.setDescricao("Salario Ref. Setembro");
@@ -28,16 +27,18 @@ public class investSimplesApp {
 
 		EntradasDao entDao = new EntradasDao(ConexaoFintech.abrirConexao());
 
-		// entDao.gravar(entrada);
+		//entDao.gravar(entrada);
 
 		// entDao.buscarEntradaPorId(1);
 
-		List<T_entradas> ent = entDao.listarTodos();
-
-		for (T_entradas selectEntrada : ent) {
-			System.out.println(selectEntrada);
-		}
-		;
+		
+		  List<T_entradas> ent = entDao.listarTodos();
+		  
+		  for (T_entradas selectEntrada : ent) 
+		  { System.out.println(selectEntrada); 
+		  
+		  };
+		 
 
 	}
 
